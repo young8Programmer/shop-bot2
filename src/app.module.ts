b36f4fa0d6aae7e0100920f43c0881163ac42bcd
Vali <valireyimbergenov79@gmail.com>
2025-05-20 17:53:45 +0500

    app.module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config'; // ConfigModule qo‘shildi
+import { ConfigModule } from '@nestjs/config';
+import { I18nModule, QueryResolver, AcceptLanguageResolver } from 'nestjs-i18n';
+import { join } from 'path';
 import { BotModule } from './bot/bot.module';
 import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
@@ -14,21 +16,26 @@ import { Product } from './entities/product.entity';
 import { Cart } from './entities/cart.entity';
 import { Order } from './entities/order.entity';
 import { Message } from './entities/message.entity';
-import * as i18n from 'i18n';
-import { join } from 'path';
 
-i18n.configure({
-  locales: ['uz', 'ru', 'en'],
-  directory: join(__dirname, 'i18n'),
-  defaultLocale: 'uz',
-  objectNotation: true,
-});
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
+
+    I18nModule.forRoot({
+      fallbackLanguage: 'uz',
+      loaderOptions: {
+        path: join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang', 'locale'] },
+        AcceptLanguageResolver,
+      ],
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: 'postgresql://postgres:HcKaqszsvlsCeyUdpqElAHvYaQwxFzEX@nozomi.proxy.rlwy.net:22265/railway',
@@ -39,6 +46,7 @@ i18n.configure({
       },
       autoLoadEntities: true,
     }),
+
     BotModule,
     UserModule,
     CategoryModule,
