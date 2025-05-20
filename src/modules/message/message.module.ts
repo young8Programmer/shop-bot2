05f143fab92ff0e4efd52a45c5155eb0a56396cf
Vali <valireyimbergenov79@gmail.com>
2025-05-20 15:58:11 +0500

    order.modul add user, product

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from '../../entities/message.entity';
 import { MessageService } from './message.service';
+import { User } from 'src/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [TypeOrmModule.forFeature([Message, User])],
   providers: [MessageService],
   exports: [MessageService],
 })
--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../../entities/order.entity';
 import { OrderService } from './order.service';
+import { User } from 'src/entities/user.entity';
+import { Product } from 'src/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, User, Product])],
   providers: [OrderService],
   exports: [OrderService],
 })
